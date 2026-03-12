0ea5d2fd7079856bca9704f81f8a8ea453c495b8
izzmejeziiii <jeziannetobio970@gmail.com>
2026-03-12 15:07:19 +0800

    ad image configs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,35 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    images: {
+        dangerouslyAllowSVG: true,
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "**",
+            },
+            {
+                protocol: "https",
+                hostname: "images.emojiterra.com",
+                pathname: "/**",
+            },
+            {
+                protocol: "https",
+                hostname: "res.cloudinary.com",
+                pathname: "/**",
+            },
+            {
+                protocol: "https",
+                hostname: "images.unsplash.com",
+                pathname: "/**",
+            },
+            {
+                protocol: "https",
+                hostname: "images.goabroad.com",
+                pathname: "/**",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
